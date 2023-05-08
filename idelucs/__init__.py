acb39862655406cac7efa9e9a119f3e32edb511d
Pablo Millan <pablito9507@gmail.com>
2023-05-08 13:33:30 -0400

    Delete idelucs directory

--- idelucs/__init__.py
+++
@@ -1,5 +0,0 @@
-from idelucs.utils import check_sequence, SummaryFasta, reverse_complement, kmer_rev_comp, kmersFasta, cgrFasta, cluster_acc, compute_results, SequenceDataset, PlotPolygon
-from idelucs.kmers import kmer_counts,cgr
-from idelucs.models import IID_model
-from idelucs.LossFunctions import IID_loss, info_nce_loss
-from idelucs.utils_GUI import define_ToolTips
